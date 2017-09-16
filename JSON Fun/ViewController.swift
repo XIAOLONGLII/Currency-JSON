b140deb00ab8ecd7eb1255e880a5a9a98eb57bd1
Garan Garan <jortiz@garan.com>
2017-09-16 12:17:43 -0400

    Initial Commit

---
+++ JSON Fun/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JSON Fun
+//
+//  Created by Xiaolong Li on 9/16/17.
+//  Copyright © 2017 Xiaolong Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
